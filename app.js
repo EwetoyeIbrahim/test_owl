2db8a4b42ece5ad61ecd908567f3f499a52a0215
EwetoyeIbrahim <i.ewetoye@gmail.com>
2021-03-31 10:18:15 +0100

    update

--- app.js
+++ app.js
@@ -1,54 +1,145 @@
+(function () {
+  const { Component } = owl;
+  const { xml } = owl.tags;
+  const { whenReady } = owl.utils;
+  
+  const { useRef, useSubEnv } = owl.hooks;
 
-/**
- * This is the javascript code defined in the playground.
- * In a larger application, this code should probably be moved in different
- * sub files.
- */
-function app() {
-  // In this example, we show how components can be defined and created.
-  const { Component, useState, mount } = owl;
+  // -------------------------------------------------------------------------
+  // Model
+  // -------------------------------------------------------------------------
+  class TaskModel extends owl.core.EventBus {
+      nextId = 1
+      tasks = [];
 
-  class Greeter extends Component {
-      constructor() {
-          super(...arguments);
-          this.state = useState({ word: 'Hello' });
+      constructor(tasks) {
+          super()
+          for (let task of tasks) {
+              this.tasks.push(task);
+              this.nextId = Math.max(this.nextId, task.id + 1);
+          }
       }
 
-      toggle() {
-          this.state.word = this.state.word === 'Hi' ? 'Hello' : 'Hi';
+      addTask(title) {
+          const newTask = {
+              id: this.nextId++,
+              title: title,
+              isCompleted: false,
+          };
+          this.tasks.unshift(newTask);
+          this.trigger('update');
+      }
+
+      toggleTask(id) {
+          const task = this.tasks.find(t => t.id === id);
+          task.isCompleted = !task.isCompleted;
+          this.tasks.sort(function (a,b) {
+              if (a.isCompleted) {
+                  if (b.isCompleted) {
+                      a.title.localeCompare(b.title)
+      } else {
+    return 1;
+      }
+  } else {
+                  if (b.isCompleted) {
+    return -1;
+      } else {
+                      a.title.localeCompare(b.title)
+      }
+  }
+    });
+          this.trigger('update')
+      }
+
+      deleteTask(id) {
+          const index = this.tasks.findIndex(t => t.id === id);
+          this.tasks.splice(index, 1);
+          this.trigger('update');
       }
   }
 
-  // Main root component
+  class StoredTaskModel extends TaskModel {
+      constructor(storage) {
+          const tasks = storage.getItem("todoapp");
+          super(tasks ? JSON.parse(tasks) : []);
+          this.on('update', this, () => {
+              storage.setItem("todoapp", JSON.stringify(this.tasks))
+          });
+      }
+  }
+
+  // -------------------------------------------------------------------------
+  // Task Component
+  // -------------------------------------------------------------------------
+  const TASK_TEMPLATE = xml /* xml */`
+    <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+      <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
+      <span><t t-esc="props.task.title"/></span>
+      <span class="delete" t-on-click="deleteTask">🗑</span>
+    </div>`;
+
+  class Task extends Component {
+      static template = TASK_TEMPLATE;
+  
+      toggleTask() {
+          this.env.model.toggleTask(this.props.task.id);
+      }
+
+      deleteTask() {
+          this.env.model.deleteTask(this.props.task.id);
+      }
+  }
+
+  // -------------------------------------------------------------------------
+  // App Component
+  // -------------------------------------------------------------------------
+  const APP_TEMPLATE = xml /* xml */`
+  <div class="todo-app">
+  <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>  
+  <div class="task-list">
+        <t t-foreach="env.model.tasks" t-as="task" t-key="task.id">
+          <Task task="task"/>
+        </t>
+     </div>
+  </div>`;
+
   class App extends Component {
+      static template = APP_TEMPLATE;
+      static components = { Task };
+
+      inputRef = useRef("add-input");
+      
       constructor() {
-          super(...arguments);
-          this.state = useState({ name: 'World'});
+          super();
+          
+          const model = new StoredTaskModel(this.env.localStorage);
+          model.on('update', this, this.render);
+          useSubEnv({model});
       }
+
+      mounted() {
+          this.inputRef.el.focus();
+      }
+
+      addTask(ev) {
+          // 13 is keycode for ENTER
+          if (ev.keyCode === 13) {
+              const title = ev.target.value.trim();
+              ev.target.value = "";
+              if (title) {
+                  this.env.model.addTask(title);
+              }
+          }
+      }
+
   }
-  App.components = { Greeter };
 
-  // Application setup
-  mount(App, { target: document.body });
-
-}
-
-/**
- * Initialization code
- * This code load templates, and make sure everything is properly connected.
- */
-async function start() {
-  let templates;
-  try {
-    templates = await owl.utils.loadFile('app.xml');
-  } catch(e) {
-    console.error(`This app requires a static server.  If you have python installed, try 'python app.py'`);
-    return;
+  // Setup code
+  function setup() {
+    App.env.localStorage = window.localStorage;
+    const app = new App();
+    app.mount(document.body);
   }
-  const env = { qweb: new owl.QWeb({templates})};
-  owl.Component.env = env;
-  await owl.utils.whenReady();
-  app();
-}
-
-start();
+  
+  whenReady(setup);
+})();
